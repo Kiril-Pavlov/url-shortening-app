dea8627f4a65e9da1af8899ba3075f527ddff170
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-30 19:05:39 +0100

    imported all the components to their place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+//components
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import MainSection from "./components/MainSection/MainSection"
+import Statistics from "./components/Statistics/Statistics"
+import Boost from "./components/Boost/Boost"
+import Footer from "./components/Footer/Footer"
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Working...</h1>
+      <Header />
+      <Hero />
+      <MainSection />
+      <Statistics />
+      <Boost />
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Navbar from "../Navbar/Navbar"
+
 import styles from "./Header.module.css"
 
 const Header = () => {
   return (
     <header className='headerContainer'>
-        
+        <Navbar />
     </header>
   )
 }
