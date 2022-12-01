70e291128c4b9859a2fe0368ad92e939267067dd
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-01 19:09:27 +0100

    hamburger toggle button done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Navbar from "../Navbar/Navbar"
 
 import styles from "./Header.module.css"
 
 import Logo from "../../assets/logo.svg"
+import Hamburger from "../../assets/hamburger.svg"
+import Close from "../../assets/close-menu.svg"
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   const links = [
     {linkName:"features"},
     {linkName:"pricing"},
     {linkName:"resources"},
   ]
+
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <header className={styles.headerContainer}>
@@ -22,6 +30,13 @@ const Header = () => {
       <div className={styles.loginSignUp}>
         <button className={styles.loginBtn}>Login</button>
         <button className={styles.signUpBtn}>Sign Up</button>
+      </div>
+      <div className={styles.hamburgerContainer}>
+        {isOpen ? (
+          <img src={Close} alt="close" className={styles.closeIcon} onClick={toggleIsOpen}/>
+        ):(
+          <img src={Hamburger} alt="hamburger" className={styles.hamburgerIcon} onClick={toggleIsOpen}/>
+        )}
       </div>
     </header>
   )
