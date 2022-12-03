159c8c66fdd2505595ad1a7e1d42764a22070cc7
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-03 23:29:51 +0100

    footer for desktop done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,52 @@ import React from 'react'
 
 import styles from "./Footer.module.css"
 
+import Logo from "../../assets/logo.svg"
+import FBicon from "../../assets/icon-facebook.svg"
+import TwitterIcon from "../../assets/icon-twitter.svg"
+import PinterestIcon from "../../assets/icon-pinterest.svg"
+import IGicon from "../../assets/icon-instagram.svg"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footerContainer}>
+      <div className={styles.footerContent}>
+        <div className={styles.footerLogoContainer}>
+          <img src={Logo} alt="Logo" className={styles.footerLogo}/>
+        </div>
+        <div className={styles.footerLinksList}>
+          <div className={styles.footerSubtitle}>Features</div>
+          <ul className={styles.footerSubList}>
+            <li>Link Shortening</li>
+            <li>Branded Links</li>
+            <li>Analytics</li>
+          </ul>
+        </div>
+        <div className={styles.footerLinksList}>
+          <h3 className={styles.footerSubtitle}>Resources</h3>
+          <ul className={styles.footerSubList}>
+            <li>Blog</li>
+            <li>Developers</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div className={styles.footerLinksList}>
+          <h3 className={styles.footerSubtitle}>Company</h3>
+          <ul className={styles.footerSubList}>
+            <li>About</li>
+            <li>Our Team</li>
+            <li>Careers</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.footerSocialIcons}>
+          <img src={FBicon} alt="Facebook" className={styles.footerSocialIcon}/>
+          <img src={TwitterIcon} alt="Twitter" className={styles.footerSocialIcon}/>
+          <img src={PinterestIcon} alt="Pinterest" className={styles.footerSocialIcon}/>
+          <img src={IGicon} alt="Instagram" className={styles.footerSocialIcon}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
