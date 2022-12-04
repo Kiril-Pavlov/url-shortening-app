ba01ecdad26c20d1a269a2339ebd5aa1318ccbc2
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-04 21:00:32 +0100

    Boost component desktop done

--- src/components/Boost/Boost.jsx
+++ src/components/Boost/Boost.jsx
@@ -4,7 +4,10 @@ import styles from "./Boost.module.css"
 
 const Boost = () => {
   return (
-    <div>Boost</div>
+    <div className={styles.boostContainer}>
+      <div className={styles.boostTitle}>Boost your links today</div>
+      <button className={styles.boostButton}>Get Started</button>
+    </div>
   )
 }
 
