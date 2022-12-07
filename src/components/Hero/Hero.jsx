b9258a6d2a628d900cd909f840426efa792133ca
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-07 23:12:02 +0100

    Hero Section JSX done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 import styles from "./Hero.module.css"
 
+import heroImg from "../../assets/illustration-working.svg"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className={styles.heroContainer}>
+      <div className={styles.heroLeft}>
+        <h1 className={styles.heroTitle}>More than just shorter links</h1>
+        <p className={styles.heroParagraph}>Bild your brand's recognition and get detailed insight on how your links are performing</p>
+        <button className={styles.heroButton}>Get Started</button>
+      </div>
+      <div className={styles.heroRight}>
+        <img src={heroImg} alt="Illustration Working" />
+      </div>
+    </div>
   )
 }
 
