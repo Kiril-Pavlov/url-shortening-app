b68e20a53bfacac218b40c0dbc6a717b403d8a84
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-09 18:31:21 +0100

    created JSX and styles for inactive main section

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -4,7 +4,12 @@ import styles from "./MainSection.module.css"
 
 const MainSection = () => {
   return (
-    <div>MainSection</div>
+    <main className={styles.mainSectionOuterContainer}>
+      <div className={styles.mainSectionContainer}>
+        <input type="text" className={styles.mainSectionInput} placeholder="Shorten a link here..."/>
+        <button className={styles.mainSectionBtn}>Shorten it!</button>
+      </div>
+    </main>
   )
 }
 
