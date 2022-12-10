c367b84d91758c29c2eb153006bc72d0b938387d
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-10 20:45:10 +0100

    added input state to MainSction component

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 import styles from "./MainSection.module.css"
 
 const MainSection = () => {
+
+  const [urlInput,setUrlInput] = useState("")
+
   return (
     <main className={styles.mainSectionOuterContainer}>
       <div className={styles.mainSectionContainer}>
-        <input type="text" className={styles.mainSectionInput} placeholder="Shorten a link here..."/>
+        <input 
+            type="text" 
+            className={styles.mainSectionInput} 
+            placeholder="Shorten a link here..."
+            value={urlInput} 
+            onChange={(e)=>setUrlInput(e.target.value)}/>
         <button className={styles.mainSectionBtn}>Shorten it!</button>
       </div>
     </main>
