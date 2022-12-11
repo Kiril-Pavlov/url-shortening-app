1efd812db2811111188a44129e18b29afbf686ee
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-11 17:08:10 +0100

    made an api call. fixed add bloker error

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 import styles from "./MainSection.module.css"
 
 const MainSection = () => {
 
-  const [urlInput,setUrlInput] = useState("")
+  const [urlInput, setUrlInput] = useState("")
+
+  const getShortLink = () =>{
+    axios.get(`https://api.shrtco.de/v2/shorten?url=${urlInput}`)
+    .then(res=>{
+      console.log(res.data.result)
+    })
+  }
 
   return (
     <main className={styles.mainSectionOuterContainer}>
       <div className={styles.mainSectionContainer}>
-        <input 
-            type="text" 
-            className={styles.mainSectionInput} 
-            placeholder="Shorten a link here..."
-            value={urlInput} 
-            onChange={(e)=>setUrlInput(e.target.value)}/>
-        <button className={styles.mainSectionBtn}>Shorten it!</button>
+        <input
+          type="text"
+          className={styles.mainSectionInput}
+          placeholder="Shorten a link here..."
+          value={urlInput}
+          onChange={(e) => setUrlInput(e.target.value)}
+        />
+        <button
+          className={styles.mainSectionBtn}
+          onClick={getShortLink}
+        >
+          Shorten it!
+        </button>
       </div>
     </main>
   )
